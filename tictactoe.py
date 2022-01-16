1db5fdb9df215ad314983ee49465118fa0b2f38c
deepakvictor <deepakvictor7585@gmail.com>
2022-01-16 16:55:10 +0530

    v0.0.1

--- tictactoe.py
+++ tictactoe.py
@@ -7,21 +7,60 @@ def pri():
         print("{fir} | {sec} | {thi}".format(fir = board[i],sec = board[i+1],thi = board[i+2]))
 
 def isempty(position):
-    if board[position] != '_':
+    if board[position] == '_':
         return True
     return False
-pri()
 
 def checkdraw():
-    for key,values in board:
-        if values == '_':
+    for key in board:
+        if board[key] == '_':
             return False
     return True
 
 def checkwin():
     if board[1]==board[2] and board[1]==board[3] and board[1] != '_':
         return True
+    elif board[4]==board[5] and board[4]==board[6] and board[4] != '_':
+        return True
+    elif board[7]==board[8] and board[7]==board[9] and board[7] != '_':
+        return True
     elif board[1]==board[4] and board[1]==board[7] and board[1] != '_':
         return True
-    elif board
+    elif board[2]==board[5] and board[2]==board[8] and board[2] != '_':
+        return True
+    elif board[3]==board[6] and board[3]==board[9] and board[3] != '_':
+        return True
+    elif board[1] == board[4] and board[1] == board[9] and board[1] != '_':
+        return True
+    elif board[3] == board[5] and board[3] == board[7] and board[3] != '_':
+        return True
+    else:
+        return False
+
+def ins(letter,position):
+    if isempty(position):
+        board[position] = letter
+        pri()
+        if checkdraw():
+            print("Draw")
+            exit()
+        if checkwin():
+            if letter == 'x':
+                print("X wins ")
+                exit()
+            else:
+                print("O wins ")
+                exit()
+            return
+    else:
+        print("Full")
+
+
+pri()
+for i in range(3):
+    g = int(input("Enter : "))
+    p = input("Enter pos : ")
+    ins(p,g)
+
+
 
